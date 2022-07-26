c65edb2958735d591caa227509ca3436c44953a9
Anna <nka2402@gmail.com>
2022-07-26 19:47:14 +0300

    Add gitignore

---
+++ 43/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+Console.Write("Введите значение k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Прямые пересекаются в точке с координатами ({x};{y})");
+
